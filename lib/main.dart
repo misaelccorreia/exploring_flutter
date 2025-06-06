dd10dfa1b07d4e91cf34e061427dce4716a68c27
Misael Correia <misaelccorreia@gmail.com>
2025-06-06 02:21:56 -0300

    make a rock in rio app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 
-void main() => runApp(MaterialApp(home: MyRiveAnimation()));
+void main() => runApp(const RockInRio());
 
-class MyRiveAnimation extends StatelessWidget {
-  const MyRiveAnimation({super.key});
+class RockInRio extends StatelessWidget {
+  const RockInRio({Key? key}):super(key: key);
 
+  //override para o que precisa indicar
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: RiveAnimation.network(
-          'https://cdn.rive.app/animations/vehicles.riv',
-          fit: BoxFit.cover,
-        ),
-      ),
+  Widget build(BuildContext){
+    return MaterialApp(
+      title: 'Rock in Rio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const HomePage(),
     );
   }
 }
+
+class HomePage extends StatefulWidget{
+ const HomePage({Key? key}): super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title: const Text('Atrações'),),
+        body: const Placeholder(),
+    );
+  }
+}
+
+class Atracao{
+  final String nome;
+  final int dia;
+  final List<String> tags;
+
+  const Atracao(this.nome, this.dia, this.tags);
+
+}
+
+const listaAtracoes = Atracao("Iron", 2, ["Show"]);
