65cd442a83e5e2aa38bf7acd47b7d41c96bbe1da
Misael Correia <misaelccorreia@gmail.com>
2025-06-08 14:36:50 -0300

    the dart compiler read all (dobre error)

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const RockInRio());
+void main(){
+  runApp(BookApp());
+}
 
-class RockInRio extends StatelessWidget {
-  const RockInRio({Key? key}):super(key: key);
-  //opcional mas por boas praticas do linter apresentamos o construtor
-
-
-  //override para o que precisa indicar
+class BookApp extends StatelessWidget{
   @override
-  Widget build(BuildContext){
-    return MaterialApp(
-      title: 'Rock in Rio',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const HomePage(),
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget{
- const HomePage({Key? key}): super(key: key);
+  const HomePage({Key? key}):super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState()=> _HomePageState();
 }
 
 class _HomePageState extends State<HomePage>{
-  final List<Atracao> _listaFavoritos = [];
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: const Text('Atrações'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            const TextField(),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.search),
+              label: const Text('Pesquisar')),
+            const SizedBox(height: 16),
+            Text('Foram encontrados X livros sobre X:' ,
+            style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-        body: ListView.builder(
-          itemCount: listaAtracoes.length,
-          itemBuilder: (context, index){
-            final isFavorito = _listaFavoritos.contains(listaAtracoes[index]);
-            return ListTile(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:(context)=>
-                      AtracaoPage(atracao: listaAtracoes[index])));
-              },
-              title: Text(listaAtracoes[index].nome),
-              subtitle: Wrap(
-                spacing: 8,
-                runSpacing: 4,
-                children: listaAtracoes[index]
-                    .tags
-                    .map((tag) => Chip(label: Text('#$tag')))
-                    .toList(),
-                ), 
-              leading: CircleAvatar(
-                child: Text('${listaAtracoes[index].dia}'),
-              ),
-              trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    if(isFavorito){
-                    _listaFavoritos.remove(listaAtracoes[index]);
-                    } else {
-                      _listaFavoritos.add(listaAtracoes[index]);
-                    };
-                  });
-                },
-                icon: isFavorito
-                  ? const Icon(Icons.favorite, color: Colors.red,)
-                  : const Icon(Icons.favorite_border),
-               ),
-             );  
-          }),
+      ),
     );
   }
 }
 
-class Atracao{
-  final String nome;
-  final int dia;
-  final List<String> tags;
-
-  const Atracao(this.nome, this.dia, this.tags);
-  
-}
-  
-//por inferencia o compilador de Dart admite como uma lista de objetos Atracao
-const listaAtracoes = [
-  Atracao("Iron Maiden", 2, ["Show"]),
-  Atracao("Alok", 3, ["Influente", "Top", "Show"]),
-  Atracao("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
-  Atracao("Guns N’ Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
-  Atracao("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
-  Atracao("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
-  Atracao("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
-  Atracao("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
-  Atracao("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
-  Atracao("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
-  Atracao("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
-  Atracao("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
-  ];
-
-  class AtracaoPage extends StatelessWidget{
-    final Atracao atracao;
-    const AtracaoPage({Key? key, required this.atracao}):super(key: key);
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(atracao.nome),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Placeholder(),
-        ),
-      );
-    }
-  }
