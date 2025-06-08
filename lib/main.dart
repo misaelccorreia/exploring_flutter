ba4886c0375c961e11f5ac34ad1cae2b008bd6a2
Misael Correia <misaelccorreia@gmail.com>
2025-06-08 16:43:05 -0300

    step 4 api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+//faz a serialização e a desserialização do json
+import 'dart:convert' as convert;
+
+//faz chamada para api rest
+import 'package:http/http.dart' as http;
 
 void main(){
   runApp(BookApp());
@@ -21,6 +26,22 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  void _buscarLivros() async {
+    final url = Uri.http(
+      'www.googleapi.com',
+      '/book/v1/volumes',
+      {'q':'{http}'},
+    );
+    final response = await http.get(url);
+
+    if(response.statusCode == 200){
+      final jsonResponse = convert.jsonDecode(response.body);
+      final itemCount = jsonResponse['totalItems'];
+      print('Number of books about HTTP: $itemCount.');
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +53,7 @@ class _HomePageState extends State<HomePage>{
             const TextField(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
-              onPressed: (){},
+              onPressed: _buscarLivros,
               icon: const Icon(Icons.search),
               label: const Text('Pesquisar')),
             const SizedBox(height: 16),
