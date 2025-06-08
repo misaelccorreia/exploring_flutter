cad22ae2e144e5c2dce60fd65f6d5f5467d466ae
Misael Correia <misaelccorreia@gmail.com>
2025-06-08 00:38:19 -0300

    add favorite icon and see the mutable state

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,8 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  final List<Atracao> _listaFavoritos = [];
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -36,10 +38,36 @@ class _HomePageState extends State<HomePage>{
         body: ListView.builder(
           itemCount: listaAtracoes.length,
           itemBuilder: (context, index){
+            final isFavorito = _listaFavoritos.contains(listaAtracoes[index]);
             return ListTile(
               title: Text(listaAtracoes[index].nome),
-            );
-        }),
+              subtitle: Wrap(
+                spacing: 8,
+                runSpacing: 4,
+                children: listaAtracoes[index]
+                    .tags
+                    .map((tag) => Chip(label: Text('#$tag')))
+                    .toList(),
+                ), 
+              leading: CircleAvatar(
+                child: Text('${listaAtracoes[index].dia}'),
+              ),
+              trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    if(isFavorito){
+                    _listaFavoritos.remove(listaAtracoes[index]);
+                    } else {
+                      _listaFavoritos.add(listaAtracoes[index]);
+                    };
+                  });
+                },
+                icon: isFavorito
+                  ? const Icon(Icons.favorite, color: Colors.red,)
+                  : const Icon(Icons.favorite_border),
+               ),
+             );  
+          }),
     );
   }
 }
